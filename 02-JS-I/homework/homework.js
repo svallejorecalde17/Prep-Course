6ac2e2f934a572f72a0f2c50dbb9a3d34a95bbb1
svallejorecalde17 <sebasrecalde1702@hotmail.com>
2022-10-06 00:26:09 -0500

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -19,6 +19,7 @@ const nuevaMultiplicacion = 10 * null === 40 ;
 const nuevoModulo = 21 % 5 === null;
 
 
+
 // En los próximos 22 problemas, deberás completar la función.
 // Todo tu código irá dentro de las llaves de la función.
 // Asegúrate que usas "return" cuando la consola te lo pida.
